0f963c121f9774b659d8dc9983c5b3d0b329eef6
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2024-06-25 18:09:18 +0800

    add role and username

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Pasien Permission
+        Permission::create(['name' => 'add pasien']);
+        Permission::create(['name' => 'edit pasien']);
+        Permission::create(['name' => 'delete pasien']);
+        Permission::create(['name' => 'show pasien']);
+
+        // Antrian Permission
+        Permission::create(['name' => 'add antrian']);
+        Permission::create(['name' => 'edit antrian']);
+        Permission::create(['name' => 'delete antrian']);
+        Permission::create(['name' => 'show antrian']);
+
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = Role::create(['name' => 'Admin']);
+        $orangtua = Role::create(['name' => 'Pasien']);
+
+        $user = User::factory()->create([
+            'name' => 'admin',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $user->assignRole($role);
+        $user->givePermissionTo([
+            'add pasien',
+            'edit pasien',
+            'delete pasien',
+            'show pasien',
+
+        ]);
+
+    }
+}
