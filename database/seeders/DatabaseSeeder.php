5a3009fe77ddb2de596bf2fa52b3d25a98812c19
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2024-09-27 06:08:37 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PermissionSeeder::class,
-            LayananSeeder::class,
+            // LayananSeeder::class,
             RoleSeeder::class,
         ]);
     }
